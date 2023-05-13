042f90076a660967534eca51c1d69bb5547963a2
Leisurelyjelly <131421216+Leisurelyjelly@users.noreply.github.com>
2023-05-14 03:03:26 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+import os
 from discord.ext import commands
 import json
 
@@ -7,13 +8,39 @@ with open('setting.json','r',encoding='utf8')as jfile:
 
 intents=discord.Intents.default()
 intents.members=True
-bot = commands.Bot(command_prefix='#', intents=intents)
+intents.message_content=True
 
-@bot.event
+client = commands.Bot(command_prefix='!', intents=intents)
+
+@client.event
 async def on_ready():
-    print('Bot is online')
-@bot.command()
-async def ping(ctx):
-    await ctx.send(bot.latency)
- 
-bot.run(jdata['TOKEN'])
+    print('bot ready')
+
+@client.command()
+async def hello(ctx):
+        await ctx.send('hello wow')
+@client.command()
+async def luxin(ctx):
+    folder_path = 'C:/Users/nick6/Documents/GitHub/Tut_Bot/pic' # 將 "your/folder/path" 替換為你的資料夾路徑
+    images = os.listdir(folder_path)
+    image_path = os.path.join(folder_path, images[0]) # 取得第一張圖片的路徑
+    with open(image_path, 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+@client.command()
+async def a(ctx):
+    folder_path = 'C:/Users/nick6/Documents/GitHub/Tut_Bot/pic' # 將 "your/folder/path" 替換為你的資料夾路徑
+    images = os.listdir(folder_path)
+    image_path = os.path.join(folder_path, images[1]) # 取得第一張圖片的路徑
+    with open(image_path, 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+@client.command()
+async def b(ctx):
+    folder_path = 'C:/Users/nick6/Documents/GitHub/Tut_Bot/pic' # 將 "your/folder/path" 替換為你的資料夾路徑
+    images = os.listdir(folder_path)
+    image_path = os.path.join(folder_path, images[2]) # 取得第一張圖片的路徑
+    with open(image_path, 'rb') as f:
+        picture = discord.File(f)
+        await ctx.send(file=picture)
+client.run(jdata['TOKEN'])
